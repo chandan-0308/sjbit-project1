8109c834045eaa9c6476672460d1b8da966b026b
chandan-0308 <smileychandu142@gmail.com>
2025-07-08 15:55:04 +0530

    dsa day2

---
+++ src/chandan/mca/dsacollection/Demodsa.java
@@ -0,0 +1,83 @@
+package chandan.mca.dsacollection;
+
+
+//import java.util.ArrayList;
+//  import java.util.LinkedList;
+//import java.util.HashSet;
+//import java.util.LinkedHashSet;
+//import java.util.TreeSet;
+//import java.util.HashMap;
+//import java.util.LinkedHashMap;
+//import java.util.TreeMap;
+
+
+public class Demodsa {
+  public static void main(String[] args) {
+//array demonstration and array is good for searching not for insertion and deletion as it leds to time and space complexity
+//	  ArrayList<Integer> n=new ArrayList<>();
+//	  System.out.println("Array");
+//	  n.add(10);
+//	  n.add(20);
+//	  n.add(25);
+//	  n.add(30);
+//	  n.addFirst(5);
+//	  n.add(2,15);
+//	  System.out.println(n);
+//	  System.out.println(n.get(2));
+//	  
+//	  System.out.println("Linked list");
+//	  LinkedList <Integer> m =new LinkedList<Integer>();
+//	  m.add(100);
+//	  m.addLast(200);
+//	  System.out.println(m);
+//	  
+//	  System.out.println("Hash set");
+//	  
+//	  HashSet<Integer> l=new HashSet<Integer>();
+//	  l.add(200);
+//	  l.add(300);
+//	  l.add(400);
+//	  System.out.println(l);
+//	  
+//	  System.out.println("Linked HashSet");
+//	  LinkedHashSet<Integer> o=new LinkedHashSet<Integer>();
+//	  o.add(100);
+//	  o.add(200);
+//	  o.add(300);
+//	  o.add(50);
+//	  System.out.println(o);
+//	  
+//	  System.out.println("TreeSet");
+//	  
+//	  TreeSet<Integer> p=new TreeSet<Integer>();
+//	  p.add(50);
+//	  p.add(500);
+//	  p.add(300);
+//	  System.out.println(p);
+// 
+//	  System.out.println("HashMap");
+//	  HashMap<String,Integer> d =new HashMap<String,Integer>();
+//	  d.put("a", 50);
+//	  d.put("b", 100);
+//	  d.put("c", 150);
+//	  d.put("d", 200);
+//	  System.out.println(d.get("a"));
+//	  System.out.println(d);
+//	  
+//	  System.out.println("linkedHashMap");
+//	  LinkedHashMap<String,Integer> p=new LinkedHashMap<String,Integer>();
+//	  p.put("a", 1);
+//	  p.put("c", 3);
+//	  p.put("d", 4);
+//	  p.put("b", 2);
+//	  System.out.println(p);
+
+//	  System.out.println("TreeMap");
+//	  TreeMap<String,Integer> k=new TreeMap<String,Integer>();
+//	  k.put("a", 1);
+//	  k.put("c", 3);
+//	  k.put("d", 4);
+//	  k.put("b", 2);
+//	  System.out.println(k);
+	  }
+}
