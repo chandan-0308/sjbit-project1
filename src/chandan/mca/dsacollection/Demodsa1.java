8752a805e6d88bfcef94652c363b201722d73156
chandan-0308 <smileychandu142@gmail.com>
2025-07-08 16:27:10 +0530

    exceptions concepts

---
+++ src/chandan/mca/dsacollection/Demodsa1.java
@@ -0,0 +1,18 @@
+package chandan.mca.dsacollection;
+
+public class Demodsa1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a=10;
+       int b=0;
+       int c;
+       try {
+    	   c=a/b;
+       }
+       catch(Exception e) {
+       
+       System.out.println(e);
+	}
+	}
+}
